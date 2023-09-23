67b1ad7069718e8056eb813bfd787e49fe046e11
admin <admin@gmail.com>
2023-09-23 11:11:20 -0500

    12clases

---
+++ Puesto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rifa
+{
+    public class Puesto
+    {
+        public int Numero { get; set; }
+        public bool EstaComprado { get; set; }
+        public Comprador Comprador { get; set; }
+    }
+}
---
+++ Rifa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rifa
+{
+        public class Rifa
+        {
+            public string Descripcion { get; set; }
+            public DateTime FechaSorteo { get; set; }
+            public decimal ValorBoleta { get; set; }
+            public List Puestos { get; set; }
+        }
+}
