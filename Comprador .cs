4c5386badc39bf3a2360ab274808bcd316cc2afe
admin <admin@gmail.com>
2023-09-23 11:11:20 -0500

    clases

---
+++ Comprador .cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rifa
+{
+    public class Comprador
+    {
+        public string Nombre { get; set; }
+        public string MetodoPago { get; set; }
+        public DateTime FechaPago { get; set; }
+    }
+}
