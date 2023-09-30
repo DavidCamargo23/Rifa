29e67b9abdf5bc727cc71c2078b03d594ed5bb9b
JuanDAC <juanavila12.0308@gmail.com>
2023-09-30 10:54:11 -0500

    class

--- Puesto.cs
+++ Puesto.cs
@@ -8,7 +8,19 @@ namespace Rifa
 {
     public class Puesto
     {
-        public int Numero { get; set; }
+        public int _Numero;
+        public int Numero
+            {
+                get
+                {
+                    return _Numero;
+                }
+                set
+                {
+                    _Numero = value;
+                }
+            }
+    
         public bool EstaComprado { get; set; }
         public Comprador Comprador { get; set; }
     }
