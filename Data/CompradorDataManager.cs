4cd010a661b5d8f83ba8cdb6b040a47fc948e919
admin <admin@gmail.com>
2023-10-28 11:54:36 -0500

    getall

--- Data/CompradorDataManager.cs
+++ Data/CompradorDataManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +23,15 @@ namespace Rifa.Data
         public static List<Comprador> GetAll()
         {
             return new List<Comprador>();
+        }
+        public static Comprador GetComprador(int id)
+        {
+            Comprador comprador = new Comprador("Grabriel ", "Garcia Marquez ", new DateTime(2023, 9, 19));
+            return comprador;
+        }
+        public static bool DeleteComprador(int id)
+        {
+           return true;
         }
     }
 }
